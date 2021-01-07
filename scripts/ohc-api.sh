3aae337acae911facadb4e011604065d563fd131
sba <sailboatanon@protonmail.com>
2021-01-07 12:35:10 -0800

    fun !

--- scripts/ohc-api.sh
+++ scripts/ohc-api.sh
@@ -1,2 +1,2 @@
 # Greatly improved!
-wget https://github.com/sailboat-anon/wifi-pineapple-mark-vii/blob/main/scripts/loot-n-scoot.sh
+https://github.com/sailboat-anon/wifi-pineapple-mark-vii/blob/main/scripts/loot-n-scoot.sh
